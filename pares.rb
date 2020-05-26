c15c5f8bb71caec9575aab9343df7149c9f7b573
Felipe Guerra <fguerra.ag@gmail.com>
2020-05-26 14:06:02 -0400

    pares

--- pares.rb
+++ pares.rb
@@ -1,10 +1,11 @@
-nnumeros = ARGV[0].to_i
-sumapares = ((nnumeros - 0)/2)+1
-puts sumapares
+#Crear el programa pares.rb donde se sumen únicamente los números pares dentro del ciclo
+#entre 0 y un número ingresado por el usuario al momento de cargar el programa.
 
-=begin
-puts 'Ingrese un numero'
-nnumeros = gets.chomp.to_i
-sumapares = ((nnumeros - 0)/2)+1
-puts 'la suma de los pares hasta el numero ingresado es #{sumapares}'
-=end
+i = 0
+suma = 0
+valor = ARGV[0].to_i
+while i < valor
+    i += 2
+    suma = suma + i # i tomaria el valor de i + 1 hasta llegar al "valor"
+end
+puts suma
