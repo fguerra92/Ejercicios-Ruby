44b2023a317a23a1ac6e26f99ce560735658d629
Felipe Guerra <fguerra.ag@gmail.com>
2020-06-01 21:50:39 -0400

    patron3

--- patron3.rb
+++ patron3.rb
@@ -1,3 +1,4 @@
+=begin prueba patron ".." "**" "||" unidos
 numero = ARGV[0].to_i
 
 numero.times do |i|
@@ -7,6 +8,21 @@ numero.times do |i|
         print "**"
     else 
         print "||"
+    end
+end
+
+print "\n"
+=end
+
+n = ARGV[0].to_i
+
+n.times do |i|
+    if i%6 == 0 || i%6 == 1
+        print '.'
+    elsif i%6 == 2 || i%6 == 3
+        print '*'
+    else
+        print '|'
     end
 end
 
